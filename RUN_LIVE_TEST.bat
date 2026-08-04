5219946269cd32732024d44bb85d72b28a888474
Pytt <silverezakpa88@gmail.com>
2026-08-04 11:59:10 +0000

    chore: clean .gitignore - exclude QA scripts, coverage, screenshots and dev artifacts

--- RUN_LIVE_TEST.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo =========================================================
-echo 🚀 LANCEMENT DU TEST VISUEL EN LIVE DANS CHROME DEV
-echo =========================================================
-start "" "C:\Program Files\Google\Chrome Dev\Application\chrome.exe" --user-data-dir="C:\tmp\chrome_live_profile" --remote-debugging-port=9222 --start-maximized --new-window "http://localhost:3000"
-timeout /t 2 /nobreak
-node deep_visual_live_audit.cjs
